c2547598f6d4ddd5103dad1f03ca7fc74ca58a87
Jarod Reyes <jarodreyes@gmail.com>
2012-09-09 13:38:22 -0400

    Added actual email link to btn.

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 Mousetrap.bind('space', function(e) {
     e.preventDefault();
-    $("#watch-player").append("<div id='marker' style='width: 11px; height: 42px; position: absolute; background: green; left: 20px; z-index: 30; top: 321px;'>$$$</div>")
+    $("#watch-player").append("<div id='marker' style='width: 11px; height: 42px; position: absolute; background: green; left: 495px; z-index: 30; top: 321px;'>$$$</div>")
 });
 
