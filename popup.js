63dc6ec25896dc914203c8a636d114580351165d
Abe Stanway <abestanway@gmail.com>
2012-09-09 13:01:32 -0400

    HACK HACK HACK

--- popup.js
+++ popup.js
@@ -1,2 +1,5 @@
-var e = Popcorn("#watch-video");
-e.currentTime();
+Mousetrap.bind('space', function(e) {
+    e.preventDefault();
+    $("#watch-player").append("<div id='marker' style='width: 11px; height: 42px; position: absolute; background: green; left: 20px; z-index: 30; top: 321px;'>$$$</div>")
+});
+
